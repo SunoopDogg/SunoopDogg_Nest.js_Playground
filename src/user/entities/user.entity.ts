c2965e1875d7e4d7ff5d8047642df0adfecb3f5f
SunoopDogg <aswoo55555@gmail.com>
2023-07-18 23:48:51 +0900

    feat: Define User entity schema with TypeOrm

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,40 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 60 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  phone: string;
+
+  @Column({ type: 'boolean' })
+  agree: boolean;
+
+  @Column({ type: 'varchar', length: 200, nullable: true, default: null })
+  address: string;
+
+  @Column({ type: 'enum', enum: ['admin', 'user'], default: 'user' })
+  role: string;
+
+  @Column({ type: 'enum', enum: ['active', 'inactive'], default: 'active' })
+  status: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: string;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  deleted_at: string;
+}
