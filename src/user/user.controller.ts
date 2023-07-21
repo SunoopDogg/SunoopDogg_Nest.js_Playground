9df1e89bca906fd4e8f6d94db5da35dcf94e793f
SunoopDogg <aswoo55555@gmail.com>
2023-07-22 00:36:40 +0900

    feat: Add register method to UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Crud({
   model: {
@@ -12,4 +14,9 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController implements CrudController<User> {
   constructor(public service: UserService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return await this.service.create(createUserDto);
+  }
 }
