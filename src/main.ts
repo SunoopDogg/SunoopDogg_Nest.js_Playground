f1168183e738a1e60fc500706ccf7236554e11cf
SunoopDogg <aswoo55555@gmail.com>
2023-07-21 23:44:16 +0900

    feat: Allow PATCH requests in CORS settings

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
     origin: ['http://localhost:3000', 'http://localhost:4000'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     preflightContinue: false,
     optionsSuccessStatus: 204,
