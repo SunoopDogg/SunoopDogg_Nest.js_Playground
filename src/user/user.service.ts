3762b3ba49ea0d8a3da163c523e2dd569f669d80
SunoopDogg <aswoo55555@gmail.com>
2023-07-22 00:38:28 +0900

    feat: Add hashed password creation in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
+import * as bcrypt from 'bcrypt';
+
 import { User } from './entities/user.entity';
+
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<User> {
   constructor(@InjectRepository(User) repo) {
     super(repo);
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(createUserDto.password, saltOrRounds);
+    createUserDto.password = hash;
+
+    return await this.repo.save(createUserDto);
   }
 }
