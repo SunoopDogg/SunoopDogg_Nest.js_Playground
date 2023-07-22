220a3948ab42d7a40e3056c45a60e1338249851a
SunoopDogg <aswoo55555@gmail.com>
2023-07-22 23:06:51 +0900

    feat: Add Passport and strategies in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 import { UserModule } from 'src/user/user.module';
 
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
+
 @Module({
   imports: [
-    UserModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async () => ({
@@ -18,8 +21,10 @@ import { UserModule } from 'src/user/user.module';
         signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
       }),
     }),
+    PassportModule,
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
