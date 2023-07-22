a23c5925d57bd386faee5fc2a6e03dcdf31a4c54
SunoopDogg <aswoo55555@gmail.com>
2023-07-22 21:31:13 +0900

    feat: Add signIn method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from 'src/user/user.service';
+
+import { SignInDto } from './dto/sign-in.dto';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async signIn(singInDto: SignInDto) {
+    const user = await this.userService.findOne({
+      where: { email: singInDto.email },
+    });
+
+    if (user && (await bcrypt.compare(singInDto.password, user.password))) {
+      const { password, ...result } = user;
+      password;
+      return result;
+    }
+
+    return null;
+  }
+}
